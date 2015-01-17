ed44a379a5ce1f2d90148511970d46aa228b6e1d
Justin Woo <moomoowoo@gmail.com>
2015-01-17 18:59:53 -0500

    draw trails as the cursor moves by checking for existing

--- src/etch_sketch_om/core.cljs
+++ src/etch_sketch_om/core.cljs
@@ -13,7 +13,7 @@
          y :y
          fill :fill} props]
     (reify om/IRender
-      (render [this]
+      (render [_]
         (dom/rect
           #js {:width width
                :height height
@@ -21,16 +21,33 @@
                :y y
                :fill fill})))))
 
+(defn etch-point [props]
+  "My Etch-Sketch point"
+  (reify om/IRender
+    (render [_]
+      (let [{x :x
+             y :y
+             width :width
+             height :height} props]
+        (dom/rect #js {:x x :y y :width width :height height})))))
+
+(defn etch-trail [props]
+  "My Etch-Sketch trail"
+  (reify om/IRender
+    (render [_]
+      (apply dom/g nil
+        (map (fn [point] (om/build etch-point point)) props)))))
+
 (defn etch-sketch [props]
   "My Etch-Sketch board"
-  (let [cursor (:cursor props)
-        {width :width
-         height :height} (:svg props)]
+  (let [{cursor :cursor svg :svg trail :trail} props
+        {width :width height :height} svg]
     (reify om/IRender
-      (render [this]
+      (render [_]
         (dom/svg
           #js {:width width
                :height height}
+          (om/build etch-trail trail)
           (om/build etch-cursor cursor))))))
 
 ;increment that our sketch works off of
@@ -39,55 +56,68 @@
 ;main app state
 (def app-state
   (atom
-    {:text "You ain't press nothin' yet"
-     :svg {:width 800 :height 600}
-     :cursor {:x 0 :y 0 :width increment :height increment :fill "grey"}}))
+    {:svg {:width 800 :height 600}
+     :cursor {:x 0 :y 0 :width increment :height increment :fill "grey"}
+     :trail []}))
 
 (om/root
   (fn [app owner]
-    (print app)
     (reify om/IRender
       (render [_]
         (dom/div nil
           (dom/h1 #js {:key :title} "Welcome to Etch-Sketch Land")
-          (dom/h2 #js {:key :msg} (:text app))
           (om/build etch-sketch app)))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
 (defn move-left []
+  "move cursor left"
   (let [app @app-state
         cursor (:cursor app)
         x (:x cursor)]
     (if (> x 0)
-      (swap! app-state assoc :cursor
-         (assoc cursor :x (- x increment))))))
+      (swap! app-state assoc :cursor (assoc cursor :x (- x increment))))))
 
 (defn move-right []
+  "move cursor right"
   (let [app @app-state
         width (:width (:svg app))
         cursor (:cursor app)
         x (:x cursor)]
     (if (< x (- width increment))
-      (swap! app-state assoc :cursor
-         (assoc cursor :x (+ x increment))))))
+      (swap! app-state assoc :cursor (assoc cursor :x (+ x increment))))))
 
 (defn move-up []
+  "move cursor up"
   (let [app @app-state
         cursor (:cursor app)
         y (:y cursor)]
     (if (> y 0)
-      (swap! app-state assoc :cursor
-         (assoc cursor :y (- y increment))))))
+      (swap! app-state assoc :cursor (assoc cursor :y (- y increment))))))
 
 (defn move-down []
+  "move cursor down"
   (let [app @app-state
         height (:height (:svg app))
         cursor (:cursor app)
         y (:y cursor)]
     (if (< y (- height increment))
-      (swap! app-state assoc :cursor
-         (assoc cursor :y (+ y increment))))))
+      (swap! app-state assoc :cursor (assoc cursor :y (+ y increment))))))
+
+(defn any-matching [pred coll]
+  "return true on any matching predicate, false otherwise"
+  (let [length (count coll)]
+    (cond
+      (= length 0) false
+      (= true (pred (first coll))) true
+      :else (recur pred (rest coll)))))
+
+(defn mark-trail []
+  (let [app @app-state
+        {cursor :cursor trail :trail} app
+        {x :x y :y} cursor]
+  (if (not (any-matching (fn [a] (= cursor a)) trail))
+    (swap! app-state assoc :trail (conj trail cursor)))))
 
 (def KeyCodes
   {:h 72 :left 37
@@ -95,12 +125,13 @@
    :k 75 :down 40
    :l 76 :right 39})
 
-(defn move-cursor [keyCode]
+(defn handleKeydown [keyCode]
   (let [keyCodes events/KeyCodes
         {h :h left :left
          j :j down :down
          k :k up :up
          l :l right :right} KeyCodes]
+    (mark-trail)
     (cond
       (or (= h keyCode) (= left keyCode)) (move-left)
       (or (= j keyCode) (= down keyCode)) (move-down)
@@ -112,9 +143,4 @@
   events/EventType.KEYDOWN
   (fn [e]
     (let [keyCode e.keyCode]
-      (swap! app-state assoc
-        :text
-        (str
-          "You pressed "
-          keyCode))
-      (move-cursor keyCode))))
+      (handleKeydown keyCode))))
